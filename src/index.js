86a99ca07e9139a34948c2448b9a050e5636057e
Kajal0211 <YOUR_GITHUB_EMAIL>
2025-11-17 20:55:16 +0530

    Fix React 18 index.js entry point

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/index.css';
-import './styles/animations.css';
-import './styles/mobile.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./styles/index.css";
+import "./styles/animations.css";
+import "./styles/mobile.css";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
     <App />
