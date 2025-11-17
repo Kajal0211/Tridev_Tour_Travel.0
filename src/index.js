d1c272f5fd833447c0fe9c036eeee165c51e7997
Kajal0211 <YOUR_GITHUB_EMAIL>
2025-11-17 21:09:20 +0530

    Fix homepage + router for Vercel deployment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/index.css';
+import './styles/animations.css';
+import './styles/mobile.css';
 
-import "./styles/index.css";
-import "./styles/animations.css";
-import "./styles/mobile.css";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
